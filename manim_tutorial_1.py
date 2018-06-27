44077051a444021422ee28656d0e273616332c5d
“Todd <zimmermant@uwstout.edu>
2018-06-27 11:36:31 -0500

    Includes code through part 9

--- manim_tutorial_1.py
+++ manim_tutorial_1.py
@@ -256,11 +256,12 @@ class ExampleApproximation(GraphScene):
         term_num = [
             TexMobject("n = " + str(n),aligned_edge=TOP)
             for n in range(0,8)]
-        [t.to_edge(BOTTOM,buff=SMALL_BUFF) for t in term_num]
+        #[t.to_edge(BOTTOM,buff=SMALL_BUFF) for t in term_num]
 
 
-        term = TexMobject("")
-        term.to_edge(BOTTOM,buff=SMALL_BUFF)
+        #term = TexMobject("")
+        #term.to_edge(BOTTOM,buff=SMALL_BUFF)
+        term = VectorizedPoint(3*DOWN)
 
         approx_graph = VectorizedPoint(
             self.input_to_graph_point(self.center_point, func_graph)
@@ -275,3 +276,76 @@ class ExampleApproximation(GraphScene):
                 Transform(term,term_num[n])
             )
             self.wait()
+
+
+class DrawAnAxis(Scene):
+    CONFIG = { "plane_kwargs" : { 
+        "x_line_frequency" : 2,
+        "y_line_frequency" :2
+        }
+    }
+
+    def construct(self):
+        my_plane = NumberPlane(**self.plane_kwargs)
+        my_plane.add(my_plane.get_axis_labels())
+        self.add(my_plane)
+        self.wait()
+
+class SimpleField(Scene):
+    CONFIG = {
+    "plane_kwargs" : {
+        "color" : RED
+        },
+    }
+    def construct(self):
+        plane = NumberPlane(**self.plane_kwargs) #Create axes and grid
+        plane.add(plane.get_axis_labels())  #add x and y label
+        self.add(plane)  #Place grid on screen
+
+        points = [x*RIGHT+y*UP
+            for x in np.arange(-5,5,1)
+            for y in np.arange(-5,5,1)
+            ]     #List of vectors pointing to each grid point
+
+        vec_field = []  #Empty list to use in for loop
+        for point in points:
+            field = 0.5*RIGHT + 0.5*UP   #Constant field up and to right
+            result = Vector(field).shift(point)   #Create vector and shift it to grid point
+            vec_field.append(result)   #Append to list
+
+        draw_field = VGroup(*vec_field)   #Pass list of vectors to create a VGroup
+
+
+        self.play(ShowCreation(draw_field))   #Draw VGroup on screen
+
+
+class FieldWithAxes(Scene):
+    CONFIG = {
+    "plane_kwargs" : {
+        "color" : RED_B
+        },
+    "point_charge_loc" : 0.5*RIGHT-1.5*UP,
+    }
+    def construct(self):
+        plane = NumberPlane(**self.plane_kwargs)
+        plane.main_lines.fade(.9)
+        plane.add(plane.get_axis_labels())
+        self.add(plane)
+
+        field = VGroup(*[self.calc_field(x*RIGHT+y*UP)
+            for x in np.arange(-9,9,1)
+            for y in np.arange(-5,5,1)
+            ])
+
+        self.play(ShowCreation(field))
+
+
+    def calc_field(self,point):
+        #This calculates the field at a single point.
+        x,y = point[:2]
+        Rx,Ry = self.point_charge_loc[:2]
+        r = math.sqrt((x-Rx)**2 + (y-Ry)**2)
+        efield = (point - self.point_charge_loc)/r**3
+        #efield = np.array((-y,x,0))/math.sqrt(x**2+y**2)  #Try one of these two fields
+        #efield = np.array(( -2*(y%2)+1 , -2*(x%2)+1 , 0 ))/3  #Try one of these two fields
+        return Vector(efield).shift(point)
