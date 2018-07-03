eca82913653231b8e68a321a0055e1bac356701c
“Todd <zimmermant@uwstout.edu>
2018-07-03 09:36:02 -0500

    Includes code through part 10

--- manim_tutorial_1.py
+++ manim_tutorial_1.py
@@ -349,3 +349,85 @@ class FieldWithAxes(Scene):
         #efield = np.array((-y,x,0))/math.sqrt(x**2+y**2)  #Try one of these two fields
         #efield = np.array(( -2*(y%2)+1 , -2*(x%2)+1 , 0 ))/3  #Try one of these two fields
         return Vector(efield).shift(point)
+
+class ExampleThreeD(ThreeDScene):
+    CONFIG = {
+    "plane_kwargs" : {
+        "color" : RED_B
+        },
+    "point_charge_loc" : 0.5*RIGHT-1.5*UP,
+    }
+    def construct(self):
+        self.set_camera_position(0, -np.pi/2)
+        plane = NumberPlane(**self.plane_kwargs)
+        plane.main_lines.fade(.9)
+        plane.add(plane.get_axis_labels())
+        self.add(plane)
+
+        field2D = VGroup(*[self.calc_field2D(x*RIGHT+y*UP)
+            for x in np.arange(-9,9,1)
+            for y in np.arange(-5,5,1)
+            ])
+
+
+        self.play(ShowCreation(field2D))
+        self.wait()
+        self.move_camera(0.8*np.pi/2, -0.45*np.pi)
+        self.begin_ambient_camera_rotation()
+        self.wait(6)
+
+    def calc_field2D(self,point):
+        x,y = point[:2]
+        Rx,Ry = self.point_charge_loc[:2]
+        r = math.sqrt((x-Rx)**2 + (y-Ry)**2)
+        efield = (point - self.point_charge_loc)/r**3
+        return Vector(efield).shift(point)
+
+
+class EFieldInThreeD(ThreeDScene):
+    CONFIG = {
+    "plane_kwargs" : {
+        "color" : RED_B
+        },
+    "point_charge_loc" : 0.5*RIGHT-1.5*UP,
+    }
+    def construct(self):
+        self.set_camera_position(0.1, -np.pi/2)
+        plane = NumberPlane(**self.plane_kwargs)
+        plane.main_lines.fade(.9)
+        plane.add(plane.get_axis_labels())
+        self.add(plane)
+
+        field2D = VGroup(*[self.calc_field2D(x*RIGHT+y*UP)
+            for x in np.arange(-9,9,1)
+            for y in np.arange(-5,5,1)
+            ])
+
+        field3D = VGroup(*[self.calc_field3D(x*RIGHT+y*UP+z*OUT)
+            for x in np.arange(-9,9,1)
+            for y in np.arange(-5,5,1)
+            for z in np.arange(-5,5,1)])
+
+
+
+        self.play(ShowCreation(field3D))
+        self.wait()
+        self.move_camera(0.8*np.pi/2, -0.45*np.pi)
+        self.begin_ambient_camera_rotation()
+        self.wait(6)
+
+
+    def calc_field2D(self,point):
+        x,y = point[:2]
+        Rx,Ry = self.point_charge_loc[:2]
+        r = math.sqrt((x-Rx)**2 + (y-Ry)**2)
+        efield = (point - self.point_charge_loc)/r**3
+        return Vector(efield).shift(point)
+
+    def calc_field3D(self,point):
+        x,y,z = point
+        Rx,Ry,Rz = self.point_charge_loc
+        r = math.sqrt((x-Rx)**2 + (y-Ry)**2+(z-Rz)**2)
+        #efield = (point - self.point_charge_loc)/r**3
+        efield = np.array((-y,x,z))/math.sqrt(x**2+y**2+z**2)
+        return Vector(efield).shift(point)
